12c937fb56fc83350ae5a981f750a3eba9e5eca8
avelinomiranda <avelinomiranda@aeaf.edu.pt>
2023-01-04 14:28:44 +0000

    Estrutura base

--- pages/utilizadores/listar_utilizadores.php
+++ pages/utilizadores/listar_utilizadores.php
@@ -10,6 +10,8 @@ require_once PAGE_NAVBAR;
     <div class="row">
         <div class="col">
             listar utilizadores
+            <br>
+            <a href="https://generatedata.com/generator">link para gerar dados</a>
         </div>
     </div>
 
